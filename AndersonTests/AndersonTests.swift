7fd60d5c5ce92d106006919ebb66b773f8a10c9e
illitrate <150224+illitrate@users.noreply.github.com>
2025-11-29 14:30:16 +0000

    Initial Commit

---
+++ AndersonTests/AndersonTests.swift
@@ -0,0 +1,17 @@
+//
+//  AndersonTests.swift
+//  AndersonTests
+//
+//  Created by jai nelson on 23/11/2025.
+//
+
+import Testing
+@testable import Anderson
+
+struct AndersonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
